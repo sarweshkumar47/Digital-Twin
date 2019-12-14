b403ff1274e0f0219dab20f5ae8f12cf375f7f58
sarweshkumar47 <sarweshkumar47@gmail.com>
2019-12-14 19:16:10 +0530

    Update and add functionalities on ditto client to receive events from ditto server

--- picode/digital-twin-device/raspberry_thing.py
+++ picode/digital-twin-device/raspberry_thing.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2017 Contributors to the Eclipse Foundation
 #
 # See the NOTICE file(s) distributed with this work for additional
@@ -27,11 +28,17 @@ THING_COMMAND_TOPIC = THING_ID + "/things/twin/commands"
 THING_MESSAGE_TOPIC = THING_ID + "/things/live/messages"
 
 THING_MODIFY_COMMAND_TOPIC = THING_COMMAND_TOPIC + "/modify"
-TEMPERATURE_SENSOR_PROPERTIES_PATH = "/features/TemperatureSensor/properties"
-FAN_ACTUATOR_PROPERTIES_PATH = "/features/FanActuator/properties"
-TEMPERATURE_SENSOR_SAMPLING_RATE_PATH = TEMPERATURE_SENSOR_PROPERTIES_PATH + "/samplingRate"
 
-FREQUENCY_CHANGE_EVENT_TOPIC = THING_ID + "/things/twin/events/modified"
+USER_DESIRED_TEMPERATURE_PROPERTIES_PATH = "/features/DesiredTemperature/properties"
+ACTUAL_TEMPERATURE_PROPERTIES_PATH = "/features/ActualTemperature/properties"
+COOLER_PROPERTIES_PATH = "/features/ThermoElectricCooler/properties"
+HEATER_PROPERTIES_PATH = "/features/ThermoElectricHeater/properties"
+FAN_ACTUATOR_PROPERTIES_PATH = "/features/FanActuator/properties"
+
+TEMPERATURE_SENSOR_SAMPLING_RATE_PATH = ACTUAL_TEMPERATURE_PROPERTIES_PATH + "/samplingRate"
+USER_DESIRED_TEMPERATURE_SET_PATH = USER_DESIRED_TEMPERATURE_PROPERTIES_PATH + "/setTemperature"
+
+UPDATE_EVENT_TOPIC = THING_ID + "/things/twin/events/modified"
 
 
 def get_b64_auth():
@@ -47,10 +54,14 @@ class RaspberryDemoThing:
 	def __init__(self, st_file_name):
 		sensor_actuator.start_modbus(st_file_name)
 		time.sleep(1)
+		self.desired_temperature = sensor_actuator.UserDesiredTemperatureInput()
 		self.temperature_sensor = sensor_actuator.TemperatureSensor()
+		self.cooler = sensor_actuator.Cooler()
+		self.heater = sensor_actuator.Heater()
 		self.fan_actuator = sensor_actuator.FanActuator()
 
 	def handle_websocket_message(self, message):
+		# print('handle_websocket_message: Received:' + str(message))
 		if message and 'topic' in message:
 			if message['topic'].startswith(THING_EVENT_TOPIC):
 				# handle event message
@@ -58,26 +69,59 @@ class RaspberryDemoThing:
 			elif message['topic'].startswith(THING_MESSAGE_TOPIC):
 				self.__handle_message(message)
 
-	def create_temperature_change_message(self, temperature):
+	def create_user_desired_temperature_static_message(self, desired_temperature):
 		"""
-			Create the modify message that is used to notify Ditto about a new sensor value.
-			:return: The message as a json object.
-			"""
+		Create the modify message that is used to notify Ditto about a new sensor value.
+		:return: The message as a json object.
+		"""
 		return {
 			"topic": THING_MODIFY_COMMAND_TOPIC,
-			"path": TEMPERATURE_SENSOR_PROPERTIES_PATH,
+			"path": USER_DESIRED_TEMPERATURE_PROPERTIES_PATH,
+			"value": self.desired_temperature.get_properties_json(desired_temperature)
+		}
+
+	def create_temperature_change_message(self, temperature):
+		"""
+		Create the modify message that is used to notify Ditto about a new sensor value.
+		:return: The message as a json object.
+		"""
+		return {
+			"topic": THING_MODIFY_COMMAND_TOPIC,
+			"path": ACTUAL_TEMPERATURE_PROPERTIES_PATH,
 			"value": self.temperature_sensor.get_properties_json(temperature)
 		}
 
-	def create_fan_state_change_message(self, illumination):
+	def create_cooler_state_change_message(self, cooler_state):
 		"""
-			Create the modify message that is used to notify Ditto about a new sensor value.
-			:return: The message as a json object.
-			"""
+		Create the modify message that is used to notify Ditto about a new sensor value.
+		:return: The message as a json object.
+		"""
+		return {
+			"topic": THING_MODIFY_COMMAND_TOPIC,
+			"path": COOLER_PROPERTIES_PATH,
+			"value": self.cooler.get_properties_json(cooler_state)
+		}
+
+	def create_heater_state_change_message(self, heater_state):
+		"""
+		Create the modify message that is used to notify Ditto about a new sensor value.
+		:return: The message as a json object.
+		"""
+		return {
+			"topic": THING_MODIFY_COMMAND_TOPIC,
+			"path": HEATER_PROPERTIES_PATH,
+			"value": self.heater.get_properties_json(heater_state)
+		}
+
+	def create_fan_state_change_message(self, fan_state):
+		"""
+		Create the modify message that is used to notify Ditto about a new sensor value.
+		:return: The message as a json object.
+		"""
 		return {
 			"topic": THING_MODIFY_COMMAND_TOPIC,
 			"path": FAN_ACTUATOR_PROPERTIES_PATH,
-			"value": self.fan_actuator.get_properties_json(illumination)
+			"value": self.fan_actuator.get_properties_json(fan_state)
 		}
 
 	def start_polling_temperature_fan_info(self, callback):
@@ -92,16 +136,23 @@ class RaspberryDemoThing:
 		while True:
 			try:
 				# read temperature sensor
+				desired_temp = self.desired_temperature.get_user_set_temperature()
 				temp_value = self.temperature_sensor.get_temperature()
+				cooler_state = self.cooler.get_cooler_state()
+				heater_state = self.heater.get_heater_state()
 				fan_state = self.fan_actuator.get_fan_state()
-				callback(temp_value, fan_state)
+				callback(desired_temp, temp_value, cooler_state, heater_state, fan_state)
 			except ValueError:
 				print('Error when providing temperature values. Trying again')
+			except:
+				print('General exception.')
 			finally:
 				# wait for next read
-				time.sleep(1 / self.temperature_sensor.get_sampling_rate())
+				# time.sleep(1 / self.temperature_sensor.get_sampling_rate())
+				time.sleep(1)
 
 	def __handle_event(self, event):
+		print('__handle_event: Received event')
 		if self.__is_temperature_sampling_rate_change(event):
 			"""
 			update the frequency in which the sensor values of the thing should be queried
@@ -111,7 +162,17 @@ class RaspberryDemoThing:
 			print('setting samplingRate of temperature sensor to {} Hz'.format(samplingRate))
 			self.temperature_sensor.set_sampling_rate(samplingRate)
 
+		if self.__is_user_desired_temperature_set(event):
+			"""
+					 Set the desired temperature in plc using modbus write operation
+					 :param message: the modify event that should contain the temperature value
+					 """
+			desired_temp = event['value']
+			print('setting desired temperature: {}° C to plc'.format(desired_temp))
+			self.desired_temperature.set_desired_temperature(desired_temp)
+
 	def __handle_message(self, message):
+		print('__handle_message: Received message')
 		# if self.__is_buzzer_property_change(message):
 		#     """
 		#         Activate/Deactivate the buzzer.
@@ -122,6 +183,16 @@ class RaspberryDemoThing:
 		#     self.buzzer.set_enabled(buzz)
 		pass
 
+	def __is_user_desired_temperature_set(self, message):
+		"""
+		Check if the message is for setting the desired temperature.
+		:return: True if it is such kind of a message
+		"""
+		return 'path' in message \
+		       and 'value' in message \
+		       and UPDATE_EVENT_TOPIC == message['topic'] \
+		       and USER_DESIRED_TEMPERATURE_SET_PATH == message['path']
+
 	def __is_temperature_sampling_rate_change(self, message):
 		"""
 		Check if the message is for changing the frequency in which sensor values are requested.
@@ -129,5 +200,5 @@ class RaspberryDemoThing:
 		"""
 		return 'path' in message \
 		       and 'value' in message \
-		       and FREQUENCY_CHANGE_EVENT_TOPIC == message['topic'] \
+		       and UPDATE_EVENT_TOPIC == message['topic'] \
 		       and TEMPERATURE_SENSOR_SAMPLING_RATE_PATH == message['path']
--- picode/digital-twin-device/sensor_actuator_value.py
+++ picode/digital-twin-device/sensor_actuator_value.py
@@ -2,6 +2,32 @@ from datetime import datetime
 import custom_modbus_monitoring as monitor
 
 
+class UserDesiredTemperatureInput:
+
+	def __init__(self, temp_in_celsius=0):
+		self.temp_in_celsius = temp_in_celsius
+		self.desired_temp_mod_loc = '%MW0'
+		self.reset_system = '%MW1'
+
+	def get_user_set_temperature(self):
+		monitor.modbus_monitor()
+		user_set_temperature = monitor.debug_vars[0].value
+		return user_set_temperature
+
+	def set_desired_temperature(self, temp_in_celsius):
+		self.temp_in_celsius = temp_in_celsius
+		self.__write_desired_temperature_modbus_plc(self.temp_in_celsius)
+
+	def get_properties_json(self, temperature):
+		return {
+			"setTemperature": temperature
+		}
+
+	def __write_desired_temperature_modbus_plc(self, temperature):
+		monitor.write_modbus_register(self.desired_temp_mod_loc, temperature)
+		monitor.write_modbus_register(self.reset_system, 1)
+
+
 class TemperatureSensor:
 	"""A simple abstraction for using the temperature sensor"""
 	lastUpdate = None
@@ -18,7 +44,7 @@ class TemperatureSensor:
 		# 	print('Type: ' + debugs.type)
 		# 	print ('Value: ' + str(debugs.value))
 		# 	print('')
-		temp_value = monitor.debug_vars[0].value
+		temp_value = monitor.debug_vars[1].value
 		return temp_value
 
 	def get_sampling_rate(self):
@@ -32,9 +58,55 @@ class TemperatureSensor:
 
 	def get_properties_json(self, temperature):
 		return {
-			"temperatureValue": temperature,
+			"temperatureSampledValue": temperature,
 			"lastUpdate": self.get_last_update(),
 			"samplingRate": self.get_sampling_rate()
+		}
+
+
+class Cooler:
+	"""A simple abstraction for using the peltier coolers"""
+	lastUpdate = None
+
+	def __init__(self):
+		pass
+
+	def get_cooler_state(self):
+		self.lastUpdate = datetime.now().__str__()
+		monitor.modbus_monitor()
+		cooler_state = monitor.debug_vars[4].value  # Check this value
+		return cooler_state
+
+	def get_last_update(self):
+		return self.lastUpdate
+
+	def get_properties_json(self, cooler_state):
+		return {
+			"coolerState": cooler_state,
+			"lastUpdate": self.get_last_update()
+		}
+
+
+class Heater:
+	"""A simple abstraction for using the peltier coolers"""
+	lastUpdate = None
+
+	def __init__(self):
+		pass
+
+	def get_heater_state(self):
+		self.lastUpdate = datetime.now().__str__()
+		monitor.modbus_monitor()
+		heater_state = monitor.debug_vars[3].value  # Check this value
+		return heater_state
+
+	def get_last_update(self):
+		return self.lastUpdate
+
+	def get_properties_json(self, heater_state):
+		return {
+			"heaterState": heater_state,
+			"lastUpdate": self.get_last_update()
 		}
 
 
@@ -48,16 +120,21 @@ class FanActuator:
 	def get_fan_state(self):
 		self.lastUpdate = datetime.now().__str__()
 		monitor.modbus_monitor()
-		fan_state = monitor.debug_vars[1].value
+		fan_state = monitor.debug_vars[5].value
 		return fan_state
 
 	def get_last_update(self):
 		return self.lastUpdate
 
+	def get_fan_rpm(self):
+		fan_rpm = monitor.debug_vars[2].value
+		return fan_rpm
+
 	def get_properties_json(self, fan_state):
 		return {
 			"fanState": fan_state,
 			"lastUpdate": self.get_last_update(),
+			"fanRpm": self.get_fan_rpm()
 		}
 
 
